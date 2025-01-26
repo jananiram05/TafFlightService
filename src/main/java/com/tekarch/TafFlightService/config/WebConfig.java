f89f94ad38e6c43ca619679ceed12f214f1a09a3
Janani B <jananib@tekarch.com>
2025-01-26 12:00:45 -0500

    Added webconfig and updated flight controller

---
+++ src/main/java/com/tekarch/TafFlightService/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.tekarch.TafFlightService.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Match all endpoints
+                .allowedOrigins("http://127.0.0.1:5500") // Allow requests from your frontend origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+        System.out.println("CORS configuration applied.");
+    }
+}
+
--- src/main/java/com/tekarch/TafFlightService/controller/FlightController.java
+++ src/main/java/com/tekarch/TafFlightService/controller/FlightController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://127.0.0.1:5500") // Frontend origin
 @RequestMapping("/flights")
 public class FlightController {
 
